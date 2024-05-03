b0445ea6dfcbc596a7952a1337ed18c0a0e48da8
Mateus Robers <robersmateus@gmail.com>
2024-05-03 15:44:47 -0300

    updating

--- banco/bank.py
+++ banco/bank.py
@@ -2,23 +2,74 @@
 Este poderá ser capaz de criar clientes e contas
 cada cliente possui nome, cpf e idade
 e cada conta possui um client, um saldo, limite, sacar, depositar e consultar saldo'''
-import random
 import csv
 
 class Cliente():
-    def __init__(self, nome, cpf, idade, conta, saldo):
+    def __init__(self, nome, cpf, idade):
         self.nome = nome
         self.cpf = cpf
         self.idade = idade
-        self.conta = conta
-        self.saldo = saldo
     
-class Conta(Cliente):
-    def criar_conta(self, conta):
-        self.conta = random.randint(10000000000,99999999999)
-        print(conta)
+    def validar():
+        while True:
+                nome = input('Informe seu nome:\n-> ')
+                if nome.isdigit():
+                        print('Digite apenas letras.')
+                        continue
+                else:
+                        break
+        while True:
+                cpf = input('Informe seu CPF sem "-" e ".":\n-> ')
+                if len(cpf) != 11:
+                        print('CPF inválido. O CPF deve conter 11 números.')
+                        continue
+                elif cpf.isalpha():
+                        print('O CPF deve conter apenas algorismos numéricos.')
+                        continue
+                else:
+                        break
+        while True:
+                try:
+                        idade = int(input('Informe sua idade:\n-> '))
+                        if idade < 18:
+                                print('A idade mínima para criar uma conta é 18 anos.')
+                                exit()
+                        else:
+                                break
 
-    # def acessar_conta(self, conta):
-    #     for i in contas:
+                except ValueError:
+                        print('Digite apenas números')
+                        continue
+                
+        return [nome, cpf, idade]
 
-#def salvar_conta():
+    def criar_conta(self):
+        
+        num_conta = 1000000
+        with open("/Users/rober/Desktop/Estudos/SOLYD/banco/contas.csv", mode='r') as arquivo:
+               leitor_csv = csv.reader(arquivo)
+               for linha in leitor_csv:
+                      num_conta += 1
+                                      
+
+        nova_conta = [self.nome, self.cpf, self.idade, num_conta]
+        with open("/Users/rober/Desktop/Estudos/SOLYD/banco/contas.csv", mode='a', newline="") as arquivo:
+            escritor_csv = csv.writer(arquivo)
+            escritor_csv.writerow(nova_conta)
+
+    def acessar_conta():
+        conta = int(input("Digite o número da sua conta:\n-> "))
+        with open("/Users/rober/Desktop/Estudos/SOLYD/banco/contas.csv", mode='r') as arquivo:
+            leitor_csv = csv.reader(arquivo)
+            for linha in leitor_csv:
+                print(linha[3])
+                print()
+                if int(linha[3]) == conta:
+                    print(f"Olá {linha[0]}\ncpf: {linha[1]}\nidade: {linha[2]}")
+                    return
+            print("Conta não existente")
+                        
+                      
+                                      
+
+# def salvar_conta():
--- banco/main.py
+++ banco/main.py
@@ -1,41 +1,15 @@
-from bank import Conta
-import random
+from bank import Cliente
+import csv
 
 print("[1] Criar nova conta\n[2] Acessar conta já existente")
 resp = int(input("-> "))
 
 if resp == 1:
-        while True:
-                nome = input('Informe seu nome:\n-> ')
-                if nome.isdigit():
-                        print('Digite apenas letras.')
-                        continue
-                else:
-                        break
-        while True:
-                cpf = input('Informe seu CPF sem "-" e ".":\n-> ')
-                if len(cpf) != 11:
-                        print('CPF inválido. O CPF deve conter 11 números.')
-                        continue
-                elif cpf.isalpha():
-                        print('O CPF deve conter apenas algorismos numéricos.')
-                        continue
-                else:
-                        break
-        while True:
-                try:
-                        idade = int(input('Informe sua idade:\n-> '))
-                        if idade < 18:
-                                print('A idade mínima para criar uma conta é 18 anos.')
-                                exit()
-                        else:
-                                break
+        validacao = Cliente.validar()
+        cliente1 = Cliente(validacao[0], validacao[1], validacao[2])
+        cliente1.criar_conta()
 
-                except ValueError:
-                        print('Digite apenas números')
-                        continue
-numero_conta = str(random.randint(0,9999999999)).zfill(4)
+elif resp == 2:
+        Cliente.acessar_conta()
 
-with open("contas.csv", "r") as file:
-        for linha in file:
-                dados_conta = linha.strip().split(",")
+# adicionar funções de saque, transferencia e extrato
